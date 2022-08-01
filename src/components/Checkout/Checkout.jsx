a11ddded69328c35e8a2d30f027c36245dbd60e4
barbadiego <dgbarba@gmail.com>
2022-07-31 21:36:24 -0300

    Se modifica la validación de datos de formulario.

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -2,6 +2,7 @@ import { addDoc, collection, getFirestore } from 'firebase/firestore'
 import React, { useContext, useState } from 'react'
 import { myContext } from '../CartContext'
 import './Checkout.css'
+import swal from 'sweetalert';
 
 export default function Checkout() {
     const [name, setName] = useState("")
@@ -28,10 +29,18 @@ export default function Checkout() {
      }
 
     function validateInputs(){
-        if (name.length < 3) return alert("Nombre incorrecto") 
-        if (tel.length < 8) return alert("Tel incorrecto")
-        if (!email.includes('@')) return alert("Email incorrecto")
-      handleClickBuy()
+        let nameReject = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+        let telReject = /^[0-9]/gm
+        let emailReject = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+
+        if (!nameReject.test(name) || !telReject.test(tel) || !emailReject.test(email)){
+          swal("Error en formulario", "Por favor, verificar si los datos fueron escritos correctamente.")
+        } else {
+          handleClickBuy()
+        }
+        // if (name.length < 3) return swal("El campo nombre se encuentra vacío o posee pocos caracteres. Por favor, completar.") 
+        // if (tel.length < 8) return swal("El campo teléfono se encuentra vacío o menos de 8 dígitos. Por favor, completar.")
+        // if (!email.includes('@')) return swal("El mail indicado no posee '@'. Por favor, agregar el mail en formato correcto.")
      }
 
 
@@ -43,13 +52,13 @@ export default function Checkout() {
             <p style={{margin: "5px"}}>Si no se completan los datos correctamente no se podrá avanzar.</p>
             <div className="styleForm">
               <p className="textCheckout">Nombre y apellido:</p>
-              <input className="styleInput" onChange={(e) => setName(e.target.value)} type={"text"} placeholder={"Ingresar nombre. (Min: 3 letras)"}></input>
+              <input className="styleInput" onChange={(e) => setName(e.target.value)} type={"text"} value={name} placeholder={"Solo letras. (Min: 3 letras)"}></input>
 
               <p className="textCheckout">Teléfono/Celular:</p>
-              <input className="styleInput" onChange={(e) => setTel(e.target.value)} type={"tel"} placeholder={"Ingresar teléfono. (Mín: 8 números)"}></input>
+              <input className="styleInput" onChange={(e) => setTel(e.target.value)} type={"tel"} value={tel} placeholder={"Solo números. (Mín: 8 números)"}></input>
 
               <p className="textCheckout">Dirección de correo:</p>
-              <input className="styleInput" onChange={(e) => setEmail(e.target.value)} type={"email"} placeholder={"Ingresar email. Ej: example@example.com"}></input>
+              <input className="styleInput" onChange={(e) => setEmail(e.target.value)} type={"email"} value={email} placeholder={"Ej: example@example.com"}></input>
               <button onClick={validateInputs} className="styleButtonCheckout">Confirmar pedido</button>
             </div>
           </div>
